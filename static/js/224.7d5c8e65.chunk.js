c4bfe8988daf75f1069d22fcbc63105d5405ecd7
lizaklimova <132839549+lizaklimova@users.noreply.github.com>
2023-12-09 19:26:52 +0000

    Deploying to gh-pages from @ lizaklimova/energy_react-app@f764d319c36fe45dc76cb68853826d4185910f63 🚀

---
+++ static/js/224.7d5c8e65.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[224],{224:function(e,t,r){r.r(t);var a=r(184);t.default=function(){return(0,a.jsx)("div",{children:"Fav"})}}}]);
+//# sourceMappingURL=224.7d5c8e65.chunk.js.map
