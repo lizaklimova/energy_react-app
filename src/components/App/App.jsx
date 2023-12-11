a862337ab90ec52ec46be317d3d72d63d88c8c4d
lizaklimova <132839549+lizaklimova@users.noreply.github.com>
2023-12-11 18:33:28 +0200

    hero section done

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 import SharedHeader from 'components/SharedHeader/SharedHeader';
 import { lazy } from 'react';
+import Hero from 'components/Hero/Hero';
 
 const MainPage = lazy(() => import('pages/MainPage'));
 const FavouritesPage = lazy(() => import('pages/FavouritesPage'));
@@ -8,12 +9,15 @@ const NotFoundPage = lazy(() => import('pages/NotFoundPage'));
 
 export const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<SharedHeader />}>
-        <Route index element={<MainPage />} />
-        <Route path="/favourites" element={<FavouritesPage />} />
-        <Route path="*" element={<NotFoundPage />} />
-      </Route>
-    </Routes>
+    <>
+      <Routes>
+        <Route path="/" element={<SharedHeader />}>
+          <Route index element={<MainPage />} />
+          <Route path="/favourites" element={<FavouritesPage />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Route>
+      </Routes>
+      <Hero />
+    </>
   );
 };
