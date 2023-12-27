4af1230212708d11bbc30795c313518cbcf3d813
lizaklimova <132839549+lizaklimova@users.noreply.github.com>
2023-12-27 15:30:28 +0200

    finished

--- src/services/exercises-api.js
+++ src/services/exercises-api.js
@@ -3,7 +3,6 @@ import notify from 'notification';
 
 axios.defaults.baseURL = 'https://your-energy.b.goit.study/api';
 
-// get for filter
 export async function fetchFilter(page, perPage, filter) {
   const { data } = await axios.get(
     `/filters?filter=${filter}&page=${page}&limit=${perPage}`
@@ -12,7 +11,6 @@ export async function fetchFilter(page, perPage, filter) {
   return data;
 }
 
-//   get for cards
 export async function fetchCards(page, perPage, filter, name, query = '') {
   const { data } = await axios.get(
     `/exercises?${filter}=${name}&keyword=${query}&page=${page}&limit=${perPage}`
@@ -20,25 +18,21 @@ export async function fetchCards(page, perPage, filter, name, query = '') {
   return data;
 }
 
-// get for exercise information
 export async function fetchExercise(id) {
   const { data } = await axios.get(`/exercises/${id}`);
   return data;
 }
 
-//quote of the day
 export async function fetchQuote() {
   const { data } = await axios.get('/quote');
   return data;
 }
 
-//add rating
 export async function patchRating(id, rating) {
   const { data } = await axios.patch(`/exercises/${id}/rating`, rating);
   return data;
 }
 
-//subscribe
 export async function subscribe(info) {
   try {
     const { data } = await axios.post(`/subscription`, info);
